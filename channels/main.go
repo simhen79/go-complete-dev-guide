aee5330a1e5b2b89142d19cb753af67faaa1f55e
Henry Sim <henry.sim@snode.com>
2024-02-05 12:39:58 +0200

    Adding links

--- channels/main.go
+++ channels/main.go
@@ -1,11 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	links := []string{
-		"https://google.com",
-		"https://facebook.com",
+		"http://google.com",
+		"http://facebook.com",
 		"http://stackoverflow.com",
 		"http://golang.org",
-		"http://google.co.za",
+		"http://amazon.com",
 	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is up!")
 }

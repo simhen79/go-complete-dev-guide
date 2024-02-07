8027e2f40f8d36732959e47a5d05e1da18b86162
Henry Sim <henry.sim@snode.com>
2024-02-07 13:16:31 +0200

    Adding channel comms

--- channels/main.go
+++ channels/main.go
@@ -14,17 +14,23 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
+		c <- "Might be down I think"
 		fmt.Println(link, "might be down!")
 		return
 	}
 
+	c <- "It is up"
 	fmt.Println(link, "is up!")
 }
